e02ece86b79aa407badc0d9d1438e8acd3527034
Aloknath Datta <alokdatta.uit@gmail.com>
2021-12-09 11:52:13 +0000

    Revert "Modified to sync with nfdiv"
    This reverts commit a12d5419

--- main.tf
+++ main.tf
@@ -2,11 +2,17 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  tags = merge(var.common_tags,
+    map("Team Contact", var.team_contact)
+    )
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = var.location
 
-  tags = var.common_tags
+  tags = local.tags
 }
 
 module "key-vault" {
