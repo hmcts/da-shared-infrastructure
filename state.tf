341be03444c2074c38feb8f6cc7afda8832bbba0
Aloknath Datta <alokdatta.uit@gmail.com>
2021-12-09 12:01:46 +0000

    Revert "Revert nfdiv sync"

--- state.tf
+++ state.tf
@@ -1,9 +1,14 @@
 terraform {
   backend "azurerm" {}
-}
 
-data "azurerm_subnet" "subnet_a" {
-  name                 = "core-infra-subnet-0-${var.env}"
-  virtual_network_name = "core-infra-vnet-${var.env}"
-  resource_group_name  = "core-infra-${var.env}"
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "1.6.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.25"
+    }
+  }
 }
